18fe44c6acd79e3165e95730357f35517ce10707
Mariusz Ryndzionek <mryndzionek@gmail.com>
2019-10-12 22:48:03 +0200

    Further small cleanups and adjustments

--- keyboards/handwired/g84/keymaps/default/keymap.c
+++ keyboards/handwired/g84/keymaps/default/keymap.c
@@ -22,14 +22,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define _FNONE 1
 #define _FNTWO 2
 
+enum {
+  TD_ESC_CAPS = 0
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS)
+};
+
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NORMAL] = LAYOUT_G84(
-  KC_ESC    , KC_F1     , KC_F2  , KC_F3  , KC_F4   , KC_F5, KC_F6, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11    , KC_F12 , KC_PSCR, KC_PAUSE , \
-       KC_1             , KC_2   , KC_3   , KC_4    , KC_5 , KC_6 , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS   , KC_EQL , KC_BSPC, KC_HOME  , \
-  KC_TAB    , KC_Q      , KC_W   , KC_E   , KC_R    , KC_T , KC_Y , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC   , KC_RBRC, KC_BSLS, KC_PGUP  , \
-  MO(_FNONE), KC_A      , KC_S   , KC_D   , KC_F    , KC_G , KC_H , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT   ,     KC_ENTER    , KC_PGDOWN, \
-         KC_LSFT        , KC_Z   , KC_X   , KC_C    , KC_V , KC_B , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLASH  , KC_RSFT, KC_UP  , KC_END   , \
-  KC_LCTL   , TG(_FNONE), KC_LWIN, KC_LALT, KC_GRAVE,   KC_SPACE  , KC_RALT, KC_RWIN, KC_MENU, KC_INS , KC_DEL    , KC_LEFT, KC_DOWN, KC_RIGHT),
+  TD(TD_ESC_CAPS), KC_F1     , KC_F2  , KC_F3  , KC_F4   , KC_F5, KC_F6, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11    , KC_F12 , KC_PSCR, KC_PAUSE , \
+       KC_1                  , KC_2   , KC_3   , KC_4    , KC_5 , KC_6 , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS   , KC_EQL , KC_BSPC, KC_HOME  , \
+  KC_TAB         , KC_Q      , KC_W   , KC_E   , KC_R    , KC_T , KC_Y , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC   , KC_RBRC, KC_BSLS, KC_PGUP  , \
+  MO(_FNONE)     , KC_A      , KC_S   , KC_D   , KC_F    , KC_G , KC_H , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT   ,     KC_ENTER    , KC_PGDOWN, \
+         KC_LSFT             , KC_Z   , KC_X   , KC_C    , KC_V , KC_B , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLASH  , KC_RSFT, KC_UP  , KC_END   , \
+  KC_LCTL        , TG(_FNONE), KC_LWIN, KC_LALT, KC_GRAVE,   KC_SPACE  , KC_RALT, KC_RWIN, KC_MENU, KC_INS , KC_DEL    , KC_LEFT, KC_DOWN, KC_RIGHT),
 
   [_FNONE] = LAYOUT_G84(
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS , KC_TRNS, KC_TRNS, KC_TRNS, \
